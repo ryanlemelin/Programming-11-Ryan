fc84a5f9ff0079d57db3ea44b44a820702db49f7
ryanlemelin <ryanlemelin23@gmail.com>
2022-11-06 13:36:59 -0800

    Drawing Project Hand in

---
+++ Drawing Project/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       //\t	Insert a tab in the text at this point
+        //\b	Insert a backspace at this point.
+        //\n	Insert a new line in the text at this point.
+        //\r	Insert a carriage return in the text at this point
+        //\'	Insert a single quote in the text at this point
+        //\"	Insert a double quote in the text at this point
+        //\\	Insert a backslash in the text at this point
+        System.out.println("Computer Programming 11-VlN\ttRyan Lemelin\t12, October, 2022\n* * */\\* * * * */------\\* * * * /\\ * * * *\n* * //\\\\ * * * /--------\\ * * *//\\\\* * * *\n*  ///\\\\\\  * */__________\\  * ///\\\\\\ * * * \n* /______\\ *  |-|_|--|_|-|  */______\\ * *\n*   |__|   *  |__________|  *  |__|   *");
+
+
+    }
+}
